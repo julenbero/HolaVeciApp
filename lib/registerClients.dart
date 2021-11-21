a183a631ef0843d31320f89c48f3b91582e34a83
Jessica Vanegas <vanegas.pazzo.jessica@gmail.com>
2021-11-21 14:08:49 -0500

    Avance Sprint 2

---
+++ lib/registerClients.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class registerClients extends StatefulWidget {
+  const registerClients({Key? key}) : super(key: key);
+
+  @override
+  _registerClientsState createState() => _registerClientsState();
+}
+
+class _registerClientsState extends State<registerClients> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
