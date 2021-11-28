82ba6480ab06056ec82226091a13e58e5bed6d38
Julio Bermudez <julenbero@gmail.com>
2021-11-27 22:30:17 -0500

    Menu Lateral(Drawer)

---
+++ lib/drawer_menu.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:holaveci/registerClients.dart';
+import 'package:holaveci/store_search.dart';
+
+class DrawerNavigation extends StatelessWidget {
+  const DrawerNavigation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+              accountName: Text("Nombre Usuario Hola Veci"),
+              accountEmail: Text("Correo usuario@holaveci.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.blueAccent,
+              child: Text("U",style: TextStyle(color: Colors.white, fontSize: 40),),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.manage_search),
+            title: Text("Modulo de Busqueda"),
+            onTap: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Store_Search()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person_add),
+            title: Text("Registro de Clientes"),
+            onTap: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => registerClients()));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
